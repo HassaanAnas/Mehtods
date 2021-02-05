d785cfd8918e4b723ec61484be4dedd0a4be3b34
Hassaan Anas <hassaananas@usf.edu>
2021-02-04 22:49:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Author: Hassaan Anas
+// Date: 2/4/2021
+// Project: Methods
+using System;
+
+namespace Mehtods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+               
+                // Asks the user to write their name
+                Console.WriteLine("Enter your name");
+
+                // decalring variable input 
+                string input = Console.ReadLine();
+
+                // shows the message hello with the input
+
+                Console.WriteLine("Hello!! " + input);
+
+            }
+
+            catch
+            {
+                Console.WriteLine("please enter your name");
+                
+            }
+
+           
+        }
+    }
+}
